4932432fda70e11cc1020ad754e1e948c2f13500
Fala-ai-Zentra <falaaizentra@gmail.com>
2025-04-17 07:44:29 -0300

    Delete src/routes directory

--- src/routes/fortuneOxRoutes.js
+++
@@ -1,1 +0,0 @@
-// Fortune OX Backend Route
--- src/routes/fortuneRabbitRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/fortuneRabbitController');
-
-router.post('/spin', controller.createSpin);
-router.get('/spins/:userId', controller.getUserSpins);
-
-module.exports = router;
